f00a5843ae5d9c512b95cd534629f2eec8fa9375
Beni <75453780+benib3@users.noreply.github.com>
2022-03-01 20:30:45 +0100

    Update V2

--- app/src/main/java/com/example/frizer/MainActivity.java
+++ app/src/main/java/com/example/frizer/MainActivity.java
@@ -1,16 +1,10 @@
 package com.example.frizer;
 
 import android.content.Intent;
-import android.database.SQLException;
-import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import com.example.frizer.DbHelper;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity{
 
--- app/src/main/java/com/example/frizer/TerminAddActivity.java
+++ app/src/main/java/com/example/frizer/TerminAddActivity.java
@@ -3,7 +3,6 @@ package com.example.frizer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.DatePickerDialog;
-import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
@@ -11,7 +10,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.TimePicker;
 
 import java.util.Calendar;
 
@@ -38,7 +36,7 @@ public class TerminAddActivity extends AppCompatActivity implements View.OnClick
         btnDatePicker.setOnClickListener(this);
         /*btnTimePicker.setOnClickListener(this);*/
       //Za poziiv Bena
-        findViewById(R.id.pozoviButton).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.otkaziButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 pozoviBena("+38269049111 ");
